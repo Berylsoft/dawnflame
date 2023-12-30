9d0ef13f24cf660110fbcb57b3e4d2970d14734a
stackinspector <stackinspector@126.com>
2023-12-30 18:58:01 +0800

    migrate to smol

--- server/main.rs
+++ server/main.rs
@@ -1,28 +1,30 @@
 use hyper::{server::conn::http1, service::service_fn, body::Incoming as IncomingBody, Request};
 
-#[tokio::main]
-async fn main() {
+async fn _main() {
     pretty_env_logger::init();
-    let (tx, mut rx) = tokio::sync::oneshot::channel::<()>();
-    tokio::spawn(async move {
-        let ctx = tokio_actor::spawn(dawnflame::GlobalContext::init(dawnflame::GlobalConfig {
+    // TODO how axum with hyper1 do shutdown
+    let (tx, rx) = async_channel::bounded::<()>(1);
+
+    async_global_executor::spawn(async move {
+        let ctx = actor::spawn(dawnflame::GlobalContext::init(dawnflame::GlobalConfig {
             root: &std::path::PathBuf::from(&"C:/swap/dftest"),
             db_mem_max: None,
             api_path: None,
         }));
 
-        let listener = tokio::net::TcpListener::bind(("0.0.0.0", 10027)).await.unwrap();
+        // TODO(async-net) AsyncToSocketAddrs
+        let listener = async_net::TcpListener::bind(std::net::SocketAddr::from(([0, 0, 0, 0], 10027))).await.unwrap();
         loop {
-            if let Ok(()) | Err(tokio::sync::oneshot::error::TryRecvError::Closed) = rx.try_recv() {
+            if !matches!(rx.try_recv(), Err(async_channel::TryRecvError::Empty)) {
                 log::info!("close signal received");
                 ctx.wait_close().await.unwrap();
                 break;
             }
             let (stream, remote_addr) = listener.accept().await.unwrap();
-            let io = hyper_util::rt::TokioIo::new(stream);
+            let io = smol_hyper::rt::FuturesIo::new(stream);
             let local_ref = ctx.clone();
 
-            tokio::task::spawn(async move {
+            async_global_executor::spawn(async move {
                 let service = service_fn(move |req: Request<IncomingBody>| {
                     let local_ref = local_ref.clone();
                     async move {
@@ -36,10 +38,15 @@ async fn main() {
                     }
                 });
                 http1::Builder::new().serve_connection(io, service).await.unwrap()
-            });
+            }).detach();
         }
-    });
+    }).detach();
 
-    tokio::signal::ctrl_c().await.unwrap();
-    tx.send(()).unwrap();
+    async_ctrlc::CtrlC::new().unwrap().await;
+    tx.send(()).await.unwrap();
+    log::info!("close signal sent");
+}
+
+fn main() {
+    async_global_executor::block_on(_main());
 }
