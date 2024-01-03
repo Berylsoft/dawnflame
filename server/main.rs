4302d06ab12ed11268e2f65649a7513e36860568
stackinspector <stackinspector@126.com>
2024-01-04 03:36:27 +0800

    fix shutdown

--- server/main.rs
+++ server/main.rs
@@ -1,5 +1,3 @@
-use hyper::{server::conn::http1, service::service_fn, body::Incoming as IncomingBody, Request};
-
 async fn _main() {
     pretty_env_logger::init();
     // TODO how axum with hyper1 do shutdown
@@ -15,30 +13,39 @@ async fn _main() {
         // TODO(async-net) AsyncToSocketAddrs
         let listener = async_net::TcpListener::bind(std::net::SocketAddr::from(([0, 0, 0, 0], 10027))).await.unwrap();
         loop {
-            if !matches!(rx.try_recv(), Err(async_channel::TryRecvError::Empty)) {
-                log::info!("close signal received");
+            let fut = futures_lite::future::or(async {
+                Some(listener.accept().await)
+            }, async {
+                // error also means to close
+                let res = rx.recv().await;
+                log::info!("close signal received: {:?}", res);
+                None
+            });
+            if let Some(res) = fut.await {
+                let (stream, remote_addr) = res.unwrap();
+                let io = smol_hyper::rt::FuturesIo::new(stream);
+                let local_ref = ctx.clone();
+
+                async_global_executor::spawn(async move {
+                    let service = hyper::service::service_fn(move |req: hyper::Request<hyper::body::Incoming>| {
+                        let local_ref = local_ref.clone();
+                        async move {
+                            let (header, payload) = req.into_parts();
+                            Ok::<_, core::convert::Infallible>(local_ref.request(dawnflame::IncomingRequest {
+                                time: std::time::SystemTime::now(),
+                                remote_addr,
+                                header,
+                                payload: http_body_util::BodyExt::collect(payload).await.unwrap().to_bytes(),
+                            }.into()).await.unwrap())
+                        }
+                    });
+                    hyper::server::conn::http1::Builder::new().serve_connection(io, service).await.unwrap()
+                }).detach();
+            } else {
+                // TODO still not "ctx closed" randomly
                 ctx.wait_close().await.unwrap();
                 break;
             }
-            let (stream, remote_addr) = listener.accept().await.unwrap();
-            let io = smol_hyper::rt::FuturesIo::new(stream);
-            let local_ref = ctx.clone();
-
-            async_global_executor::spawn(async move {
-                let service = service_fn(move |req: Request<IncomingBody>| {
-                    let local_ref = local_ref.clone();
-                    async move {
-                        let (header, payload) = req.into_parts();
-                        Ok::<_, core::convert::Infallible>(local_ref.request(dawnflame::IncomingRequest {
-                            time: std::time::SystemTime::now(),
-                            remote_addr,
-                            header,
-                            payload: http_body_util::BodyExt::collect(payload).await.unwrap().to_bytes(),
-                        }.into()).await.unwrap())
-                    }
-                });
-                http1::Builder::new().serve_connection(io, service).await.unwrap()
-            }).detach();
         }
     }).detach();
 
